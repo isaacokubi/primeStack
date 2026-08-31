6b8b0f0fabb3ee2e41e64fca06f3744da86f9957
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-08-31 19:04:43 +0300

    feat: add testimonial CMS routes

---
+++ server/src/routes/testimonial.routes.js
@@ -0,0 +1,1 @@
+import {Router} from 'express';import {Testimonial} from '../models/Content.js';import {mountContent} from './content.routes.js';const r=Router();mountContent(r,Testimonial,{publicQuery:{}});export default r;
